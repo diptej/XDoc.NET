79f043eaea4ac622ea2dab727ae6c0d51823d460
gnostice <info@gnostice.com>
2015-05-19 10:08:50 +0530

    Added DOCX to PDF Converter demo

---
+++ Demos/Converter/DOCXToPDFConverter/DOCXToPDF.cs
@@ -0,0 +1,19 @@
+﻿using Gnostice.Documents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOCXToPDFConverter
+{
+    class DOCXToPDF
+    {
+        static void Main(string[] args)
+        {
+            Gnostice.Documents.Framework.ActivateLicense("716E-5C8A-A157-DC33-3A20-5703-4792-B2C4-F393-B1E4-236D-2A56");
+            DocumentConverter converter = new DocumentConverter();
+            converter.ConvertToFile(@".\..\..\..\Samples\DifferentAlignments.docx", @".\..\..\..\Samples\Converted\DifferentAlignments.pdf");
+        }
+    }
+}
