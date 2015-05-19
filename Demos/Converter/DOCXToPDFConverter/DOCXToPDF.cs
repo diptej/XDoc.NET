aae3276eb533686c651fa0088698e0b171953df5
gnostice <info@gnostice.com>
2015-05-19 10:59:45 +0530

    Added code comments

--- Demos/Converter/DOCXToPDFConverter/DOCXToPDF.cs
+++ Demos/Converter/DOCXToPDFConverter/DOCXToPDF.cs
@@ -11,9 +11,14 @@ namespace DOCXToPDFConverter
     {
         static void Main(string[] args)
         {
+            //Activate license
             Gnostice.Documents.Framework.ActivateLicense("A635-604D-F82A-680E-A31A-7FEE-F0F9-2EE2");
+
+            //Create DocumentConverter instance
             DocumentConverter converter = new DocumentConverter();
-            converter.ConvertToFile(@".\..\..\..\Samples\DifferentAlignments.docx", @".\..\..\..\Samples\Converted\DifferentAlignments.pdf");
+
+            converter.ConvertToFile(@".\..\..\..\Samples\DifferentAlignments.docx", 
+@".\..\..\..\Samples\Converted\DifferentAlignments.pdf");
         }
     }
 }
